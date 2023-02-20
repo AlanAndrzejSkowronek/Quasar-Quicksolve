0fe107f336998ebf702afee55a51a420bab1c976
Alan Skowronek <alanskowronek1414@gmail.com>
2023-02-21 00:15:58 +0100

    inicio de loginPage

---
+++ src/pages/loginPage.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+    @submit="onSubmit"
+    @reset="onReset"
+    class="q-gutter-md"
+    >
+        <q-input standout="bg-primary text-white" v-model="email" filled type="email" hint="Email" />
+        <q-input standout="bg-primary text-white" v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Password">
+            <template v-slot:append>
+                <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                />
+            </template>
+        </q-input>
+        <div>
+            <q-btn label="Submit" type="submit" color="primary"/>
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+    </q-form>
+
+</div>
+</template>
+  
+<script>
+    export default {
+        setup () {
+            return {
+                email: ref(''),
+                password: ref(''),
+                isPwd: ref(true)
+            }
+        }
+    }
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,24 @@
+
+const routes = [
+  {
+    path: '/home',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/login',
+    children: [
+      { path: '', component: () => import('pages/loginPage.vue') }
+    ]
+  },
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
